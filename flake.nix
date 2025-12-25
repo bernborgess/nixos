a2d56101d3bd10e4d802bb4ddf1aae5625f8aed3
bernborgess <bernborgess@outlook.com>
2025-12-25 11:43:57 -0300

    Add flakes support to nixos-hardware

--- flake.nix
+++ flake.nix
@@ -1,14 +1,15 @@
 {
   inputs = {
-    # This is pointing to an unstable release.
-    # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
-    # i.e. nixos-24.11
     # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ self, nixpkgs, nixos-hardware, ... }: {
     nixosConfigurations.e14 = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      modules = [
+	./configuration.nix
+        nixos-hardware.nixosModules.lenovo-thinkpad-e14-amd
+      ];
     };
   };
 }
